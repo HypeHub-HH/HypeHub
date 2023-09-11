3522b7c9f0e0118286054f299f9b4186f3a1feeb
Mikołaj Zgórski <119301272+Meek0hWhy@users.noreply.github.com>
2023-09-11 10:21:15 +0200

    Added models: Account and AccountCredentials. Added type: AccountType

---
+++ HypeHub/HypeHubDAL/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using HypeHubDAL.Models.Types;
+
+namespace HypeHubDAL.Models;
+
+public class Account
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public AccountCredentials Credentials { get; set; }
+    public bool IsPrivate { get; set; }
+    public AccountTypes AccountTypes { get; set; }
+    public string? AvatarUrl {get; set;}
+    public Wardrobe Wardrobe { get; set; }
+    public List<Outfit> Outfits { get; set; }
+}
