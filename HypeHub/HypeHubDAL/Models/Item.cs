f9728477ddb612d0069b30e8faad0e65a8bfc5bf
Mariusz Woźniak <mariuszwozniak145@gmail.com>
2023-09-11 10:16:10 +0200

    Added Item model and CloathingType

---
+++ HypeHub/HypeHubDAL/Models/Item.cs
@@ -0,0 +1,32 @@
+﻿using HypeHubDAL.Models.Types;
+
+namespace HypeHubDAL.Models;
+
+public class Item
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; }
+    public Wardrobe Wardrobe { get; init; }
+    public Guid WardrobeId { get; init; }
+    public List<Outfit> Outfits { get; init; } = new();
+    public CloathingType CloathingType { get; set; }
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+    public string? Colorway { get; set; }
+    public decimal? Price { get; set; }
+    public DateTime? PurchaseDate { get; set; }
+    public List<string> ItemPhotoUrls { get; init; } = new();
+
+    public Item(string name, Wardrobe wardrobe, CloathingType cloathingType, string? brand, string? model, string? colorway, decimal? price, DateTime? purchaseDate)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Wardrobe = wardrobe;
+        CloathingType = cloathingType;
+        Brand = brand;
+        Model = model;
+        Colorway = colorway;
+        Price = price;
+        PurchaseDate = purchaseDate;
+    }
+}
---
+++ HypeHub/HypeHubDAL/Models/Types/CloathingType.cs
@@ -0,0 +1,11 @@
+﻿namespace HypeHubDAL.Models.Types;
+
+public enum CloathingType
+{
+    Head,
+    Torso,
+    Hand,
+    Legs,
+    Shoes,
+    Others
+}
