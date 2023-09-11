e652e474facc892d3ab687449c879785f47954b2
Mariusz Woźniak <mariuszwozniak145@gmail.com>
2023-09-11 10:36:03 +0200

    Added Wardrobe model

---
+++ HypeHub/HypeHubDAL/Models/Wardrobe.cs
@@ -0,0 +1,17 @@
+﻿namespace HypeHubDAL.Models;
+
+public class Wardrobe
+{
+    public Guid Id { get; init; }
+    public Account User { get; init; }
+    public Guid UserId { get; init; }
+    public List<Item> Items { get; init; } = new();
+
+    public Wardrobe(Account user, Guid userId)
+    {
+        Id = Guid.NewGuid();
+        User = user;
+        UserId = userId;
+        Items = new();
+    }
+}
