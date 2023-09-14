0bfafc312bbbb2ea7c983ae8c10ab10ece1ce0f5
Mariusz Woźniak <mariuszwozniak145@gmail.com>
2023-09-14 14:38:52 +0200

    Added AutoMapper

---
+++ HypeHub/HypeHubLogic/Mappers/MapProfile.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using HypeHubDAL.Models;
+using HypeHubDAL.Models.Relations;
+using HypeHubLogic.DTOs.Account;
+using HypeHubLogic.DTOs.AccountCredentials;
+using HypeHubLogic.DTOs.AccountItemLike;
+using HypeHubLogic.DTOs.AccountOutfitLike;
+using HypeHubLogic.DTOs.Item;
+using HypeHubLogic.DTOs.ItemImage;
+using HypeHubLogic.DTOs.Outfit;
+using HypeHubLogic.DTOs.OutfitImage;
+
+namespace HypeHubLogic.Mappers;
+
+public class MapProfile : Profile
+{
+    public MapProfile()
+    {
+        CreateMap<Account, AccountReadDTO>();
+        CreateMap<Account, AccountWithCredentialsReadDTO>();
+        CreateMap<Account, AccountWithOutfitsReadDTO>();
+        CreateMap<Account, AccountWithItemsReadDTO>();
+        CreateMap<AccountCreateDTO, Account>();
+
+        CreateMap<AccountCredentials, AccountCredentialsReadDTO>();
+        CreateMap<AccountCredentialsCreateDTO, AccountCredentials>();
+
+        CreateMap<AccountItemLike, AccountItemLikeReadDTO>();
+        CreateMap<AccountItemLikeCreateDTO, AccountItemLike>();
+
+        CreateMap<AccountOutfitLike, AccountOutfitLikeReadDTO>();
+        CreateMap<AccountOutfitLikeCreateDTO, AccountOutfitLike>();
+
+        CreateMap<Item, ItemReadDTO>();
+        CreateMap<ItemCreateDTO, Item>();
+
+        CreateMap<ItemImage, ItemImageReadDTO>();
+        CreateMap<ItemImageCreateDTO, ItemImage>();
+
+        CreateMap<Outfit, OutfitReadDTO>();
+        CreateMap<OutfitCreateDTO, Outfit>();
+
+        CreateMap<OutfitImage, OutfitImageReadDTO>();
+        CreateMap<OutfitImageCreateDTO, OutfitImage>();
+    }
+}
