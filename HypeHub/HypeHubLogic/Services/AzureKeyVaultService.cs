44043146c1ba412ea04fd0a41df27cdf1c308309
Mikołaj Zgórski <119301272+Meek0hWhy@users.noreply.github.com>
2023-11-17 17:59:10 +0100

    elo

--- HypeHub/HypeHubLogic/Services/AzureKeyVaultService.cs
+++ HypeHub/HypeHubLogic/Services/AzureKeyVaultService.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Identity;
 using Microsoft.Extensions.Configuration;
 using Azure.Security.KeyVault.Secrets;
+using Google.Apis.Auth.OAuth2;
 
 namespace HypeHubLogic.Services;
 
@@ -9,8 +10,22 @@ public static class AzureKeyVaultService
     public static async Task<string> GetSecretAsync(IConfiguration configuration, string secretName)
     {
         var vaultUri = configuration["AzureKeyVault:VaultUri"];
-        var client = new SecretClient(new Uri(vaultUri), new DefaultAzureCredential());
-        KeyVaultSecret secret = await client.GetSecretAsync(secretName);
-        return secret.Value;
+        var credentials = new DefaultAzureCredential();
+        if(credentials == null)
+        {
+            var a = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
+            var b = Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
+            var c = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
+            var credentials1 = new ClientSecretCredential(c, a, b);
+            var client = new SecretClient(new Uri(vaultUri), credentials1);
+            KeyVaultSecret secret = await client.GetSecretAsync(secretName);
+            return secret.Value;
+        }
+        else
+        {
+            var client = new SecretClient(new Uri(vaultUri), new DefaultAzureCredential());
+            KeyVaultSecret secret = await client.GetSecretAsync(secretName);
+            return secret.Value;
+        }
     }
 }
